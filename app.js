a3d6450f8e8de36ae1ecc61a34537c458b6e849e
Artem Cherenkov <art.cherenkov@gmail.com>
2021-03-27 20:48:26 +0700

    feat: install pug and add templates

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express();
-
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+app.set("view engine", "pug");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -15,7 +16,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404");
 });
 
 app.listen(3000);
