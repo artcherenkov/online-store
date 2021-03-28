afb105fa985fd6fd318d0c6b240d61a1bbfcf59f
Artem Cherenkov <art.cherenkov@gmail.com>
2021-03-28 13:14:16 +0700

    refactor: refactor product model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,20 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const pathToData = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(pathToData, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +22,13 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(pathToData, JSON.stringify(products), (err) => {});
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    return getProductsFromFile(cb);
   }
 };
