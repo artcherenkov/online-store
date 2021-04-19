b8441169406ddf61de62765fcc29024c8e8fe921
Artem Cherenkov <art.cherenkov@gmail.com>
2021-04-19 16:48:49 +0700

    refactor: clear cart model

--- models/cart.js
+++ models/cart.js
@@ -1,75 +1,9 @@
-const fs = require("fs");
-const path = require("path");
 const { getFormattedPrice } = require("../utils/common");
 
-const pathToCartData = path.join(
-  path.dirname(require.main.filename),
-  "data",
-  "cart.json"
-);
-
 module.exports = class Cart {
-  static addProduct(id, productPrice) {
-    fs.readFile(pathToCartData, (err, fileContent) => {
-      let cart = { products: [], totalPrice: 0 };
+  static addProduct(id, productPrice) {}
 
-      if (!err) {
-        cart = JSON.parse(fileContent);
-      }
+  static deleteProduct(id, price) {}
 
-      const existingProductIndex = cart.products.findIndex(
-        (product) => product.id === id
-      );
-      const existingProduct = cart.products[existingProductIndex];
-      let updatedProduct;
-
-      if (!existingProduct) {
-        updatedProduct = { id: id, qty: 1 };
-        cart.products = [...cart.products, updatedProduct];
-      } else {
-        updatedProduct = { ...existingProduct };
-        updatedProduct.qty = updatedProduct.qty + 1;
-        cart.products = [...cart.products];
-        cart.products[existingProductIndex] = updatedProduct;
-      }
-      cart.totalPrice += productPrice;
-      cart.totalPrice = Number(parseFloat(cart.totalPrice).toFixed(2));
-
-      fs.writeFile(pathToCartData, JSON.stringify(cart), (err) => {});
-    });
-  }
-
-  static deleteProduct(id, price) {
-    fs.readFile(pathToCartData, (err, fileContent) => {
-      if (err) {
-        return null;
-      }
-      const cart = JSON.parse(fileContent);
-      const updatedCart = { ...cart };
-
-      const productToDelete = updatedCart.products.find((p) => p.id === id);
-      if (!productToDelete) {
-        return;
-      }
-      const productToDeleteQty = productToDelete.qty;
-
-      updatedCart.products = updatedCart.products.filter((p) => p.id !== id);
-      updatedCart.totalPrice =
-        updatedCart.totalPrice - price * productToDeleteQty;
-      updatedCart.totalPrice = getFormattedPrice(updatedCart.totalPrice);
-
-      fs.writeFile(pathToCartData, JSON.stringify(updatedCart), (err) => {});
-    });
-  }
-
-  static getCart(cb) {
-    fs.readFile(pathToCartData, (err, fileContent) => {
-      try {
-        const cart = JSON.parse(fileContent);
-        cb(cart);
-      } catch (err) {
-        cb(null);
-      }
-    });
-  }
+  static getCart(cb) {}
 };
