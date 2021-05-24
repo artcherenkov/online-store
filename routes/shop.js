82228f168e6866a248a047a53e0d4eda8334629c
Artem Cherenkov <art.cherenkov@gmail.com>
2021-05-24 21:46:47 +0700

    refactor: add comments to shop routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,30 @@
 const express = require("express");
-
 const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
+// / => GET
 router.get("/", shopController.getIndex);
 
+// /products => GET
 router.get("/products", shopController.getProducts);
 
+// /products/:productId => GET
 router.get("/products/:productId", shopController.getProduct);
 
-router.post("/cart", shopController.postCart);
+// /cart => GET
 router.get("/cart", shopController.getCart);
 
+// /cart => POST
+router.post("/cart", shopController.postCart);
+
+// /checkout => GET
 router.get("/checkout", shopController.getCheckout);
 
+// /orders => GET
 router.get("/orders", shopController.getOrders);
 
+// /cart-delete-item => POST
 router.post("/cart-delete-item", shopController.postCartDeleteItem);
 
 module.exports = router;
