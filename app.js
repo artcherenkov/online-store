f9dc57503ef22ee09c76a7c309f7a0a7e193d19f
Artem Cherenkov <art.cherenkov@gmail.com>
2021-05-24 23:32:24 +0700

    feat: add user model

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("./utils/database");
 
+const Product = require("./models/product");
+const User = require("./models/user");
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const { get404 } = require("./controllers/404");
@@ -13,15 +16,32 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) =>
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err))
+);
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
+  // .sync({ force: true })
   .sync()
-  .then(() => {
-    app.listen(3001);
+  .then(() => User.findByPk(1))
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Artem", email: "test@gmail.com" });
+    }
+    return Promise.resolve(user);
   })
+  .then(() => app.listen(3001))
   .catch((err) => console.log(err));
